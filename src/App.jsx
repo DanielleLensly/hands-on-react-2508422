913efee3bad0bfd73e5cac71b2d5761059a3bbe3
Danielle Scheurkogel <danielle.lensly93@gmail.com>
2025-04-22 08:40:17 +0200

    End of chapter 2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
-import React, { Component } from 'react';
-
-class Welcome extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <h1>Meet the { this.props.name }</h1>;
-   }
-}
-
-const Hello = (props) => {
-  return <h1>Meet the <i>{props.name}</i></h1>
-}
+import React, { useEffect, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
+import Nav from "./components/Nav";
 
 function App() {
-  const name = 'StarGazers'
+  const [cast, setCast] = useState([]);
+  let [memberInfo, setMemberInfo] = useState(null);
+
+  async function fetchCast() {
+    const response = await fetch("cast.json");
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+
   return (
-    <div className="container">
-      <article>
+    <>
+      <Nav
+        cast={cast}
+        onChoice={(info) => {
+          setMemberInfo(info);
+        }}
+      />
+      <div className="container">
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="Stars" />
-          <Hello name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <h1>Meet the Stargazers</h1>
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <ListCast
+            cast={cast}
+            onChoice={(info) => {
+              setMemberInfo(info);
+            }}
+          />
+          {memberInfo && (
+            <Modals
+              member={memberInfo}
+              handleChange={(info) => {
+                setMemberInfo(cast[info]);
+              }}
+              handleClose={() => {
+                setMemberInfo(null);
+              }}
+            />
+          )}
         </hgroup>
-      </article>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
-export default App
+export default App;
